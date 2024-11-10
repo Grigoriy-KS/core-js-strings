e2f83188a8ff0d8db8bfce7ad54fddf5c3260f71
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2024-11-10 21:37:07 +0300

    feat: create a invertCase function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -395,8 +395,16 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((currentValue) => {
+      if (currentValue === currentValue.toUpperCase()) {
+        return currentValue.toLowerCase();
+      }
+      return currentValue.toUpperCase();
+    })
+    .join('');
 }
 
 /**
